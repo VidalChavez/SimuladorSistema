68eaac77561c2db06770265081173e5d430e8d5b
Vidal Chavez <vidal.gk@gmail.com>
2013-09-03 19:07:07 -0500

    Se comenzo a implementar loader.c -Vidal

--- Practica1/loader.c
+++ Practica1/loader.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(void)
+{
+	/*Se tiene que declarar el mapa de memoria ( va en el Shell)*/	
+	system("clear");
+	printf("lo que se leyo fue:");
+}
+
 /*Ese es el archivo del loader*/
 
 
